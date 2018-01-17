401c0fff2c0803ca4630c26d465a0bb52e6a351f
DimDim <scrat7@gmail.com>
2018-01-17 11:23:21 +0100

    maj

--- app/src/main/java/fr/m2i/sqlite_annuaire_xtiers/Contact.java
+++ app/src/main/java/fr/m2i/sqlite_annuaire_xtiers/Contact.java
@@ -12,7 +12,7 @@ import android.database.sqlite.SQLiteDatabase;
 public class Contact {
 
     final String TABLE_NAME = "contacts";
-    final String COLUMNS[] = {"id", "nom", "tel"};
+    final String COLUMNS[] = {"id", "name", "tel"};
 
     private Integer id;
     private String nom;
@@ -56,30 +56,38 @@ public class Contact {
     //On définit les methodes pour réaliser les actions possibles
     //sur les contacts:
 
-    //throws ContactNotFoundException  car cette méthode peux renvoyer une exception
-    public void selectById(Integer id) throws ContactNotFoundException {
+
+    public void selectById(Integer id) throws Exception {
 
         String where = "id = ' " + id + "'";
         select(where);
     }
 
-    public void selectByNom(Integer nom) throws ContactNotFoundException {
+    public void selectByNom(String nom) throws Exception {
 
-        String where = "name = ' " + nom + "'";
+        String where = "name = '" + nom + "'";
+
         select(where);
+
 
     }
 
-    private void select(String where) throws ContactNotFoundException {
+    //throws car cette méthode peux renvoyer une exception
+    private void select(String where) throws Exception {
 
-        Cursor cursor = db.query(TABLE_NAME, COLUMNS, where, null, null, null, null);
-
+        //Cursor cursor = db.query(TABLE_NAME, COLUMNS, where,"", null, null, null,null);
+        Cursor cursor = db.query("contacts", COLUMNS, where, null, null, null, null);
         //cursor.getCount() est le nombre d'éléments trouvé lors du query
         //si supérieur à 0 donc trouvé,
-        if (cursor.getCount() > 0) {
+
+        if (cursor.getCount() == 1) {
+            cursor.moveToFirst();
             this.id = cursor.getInt(0);
             this.nom = cursor.getString(1);
             this.tel = cursor.getString(2);
+        } else if (cursor.getCount() > 1) {
+            //création d'une exception personnalisé suite plusieurs contacts  trouvé lors du query
+            throw new ContactMultiplesException();
         } else {
             //création d'une exception personnalisé suite contact non trouvé lors du query
             throw new ContactNotFoundException();
@@ -96,6 +104,16 @@ public class Contact {
 
             //on passe un String en paramètre au constructeur de la classe mère Exception
             super("Contact non trouvé");
+        }
+
+    }
+
+    public class ContactMultiplesException extends Exception {
+
+        public ContactMultiplesException() {
+
+            //on passe un String en paramètre au constructeur de la classe mère Exception
+            super("Plusieurs contacts trouvés");
         }
 
     }
